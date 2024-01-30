3225d40bf62df91b2b10ab63dc7a7261b2adcea5
Sintijaa <ipb21.s.martinsone@vtdt.edu.lv>
2024-01-30 14:05:13 +0200

    labojumi

--- tickets/src/App.jsx
+++ tickets/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import './App.css';
-import Login from './Login';
-import Home from './Home';
+import './style/App.css';
+import Login from './components/Login';
+import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
--- tickets/src/components/Login.jsx
+++ tickets/src/components/Login.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+import '../style/Login.css';
 
 function Login() {
-  return (
-    <>
-        <h1>Ticekts login</h1>
-      
-    </>
-  );
-};
+  const [action, setAction] = useState("SignUp");
 
-export default Login
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+      {action === "SignUp" ? (
+  <div className="input">
+    <input placeholder="text" type="text" />
+  </div>
+) : null}   
+        <div className="input">
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="input">
+          <input type="password" placeholder="password" />
+        </div>
+        
+      </div>
+      {action === "Signup" ? null : <div className="forgot-password">Lost password? <span>Click here!</span></div>}
+      <div className="submit-conatiner">
+        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Signup") }}>Sign up</div>
+        <div className={action === "Signup" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+      </div> 
+    </div>
+  );
+}
+
+export default Login;
